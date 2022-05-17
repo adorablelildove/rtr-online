d80e15efdf5083514535426f2f28b52126cba693
Helviani Zebua <helviani91@gmail.com>
2022-05-17 16:44:20 +0700

    membuat component class welcome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Greet from './components/justtest/Greet';
+import Welcome from './components/justtest/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Greet></Greet>
+      <Welcome></Welcome>
     </div>
   );
 }
