6eff36f87737d7772499997f1cf47bcdbca939a5
Helviani Zebua <helviani91@gmail.com>
2022-05-23 17:32:52 +0700

    menambahkan animasi pada sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as FaIcons from 'react-icons/fa'
 
 const Sidebar = () => {
 
+  const [isOpen, setIsOpen] = useState(true);
+  const [style, setStyle] = useState("expand-btn close");
+  const collapsed = () => {
+    setStyle(isOpen ? "expand-btn show" : "expand-btn close");
+  }
+  const expanded = () => {
+    setStyle(isOpen ? "expand-btn show" : "expand-btn close");
+  }
+
   return (
     <div className='main h-100vh'>
-        <td className='collapse-btn left-0 bg-main'>
-          <a><FaIcons.FaCaretDown /></a>
-        </td>
-        <div className='sidebar'>
-            hello
-        </div>
-        <td className='expand-btn left-290 bg-main'>
+        <td className={style} onClick={() => {setIsOpen(!isOpen); expanded()}}>
           <a className='f-12 f-poppins spacing'>MENU</a>
         </td>
+
+        {/* responsive sidebar */}
+        <div className={isOpen ? "sidebar show" : "sidebar close"}>
+            hello
+        </div>
+        <td className={isOpen ? "collapse-btn show" : "collapse-btn close"} onClick={() => {setIsOpen(!isOpen); collapsed()}}>
+          <a><FaIcons.FaCaretDown /></a>
+        </td>
+        {/* responsive sidebar end */}
+        
         <div className='container'>
-            ini container
+            <h1>ini container</h1>
         </div>
     </div>
   )
