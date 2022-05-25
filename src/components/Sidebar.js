55b09ee6a04703889be4c4172f21e5e122b40064
Helviani Zebua <helviani91@gmail.com>
2022-05-25 15:51:04 +0700

    membuat submenu pada add data

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import * as FaIcons from 'react-icons/fa'
 
 const Sidebar = () => {
-
-  const [isOpen, setIsOpen] = useState(true);
+  // Expand-collapse animation sidebar
+  const [isOpen, setIsOpen] = useState(true);  
   const [style, setStyle] = useState("expand-btn close");
   const collapsed = () => {
     setStyle(isOpen ? "expand-btn show" : "expand-btn close");
@@ -11,6 +11,11 @@ const Sidebar = () => {
   const expanded = () => {
     setStyle(isOpen ? "expand-btn show" : "expand-btn close");
   }
+
+  // Change main menus 
+  const [addDataIsOpen, setAddDataIsOpen] = useState(true);  
+  const [layerListIsOpen, setLayerListIsOpen] = useState(false);
+  const [legendIsOpen, setLegendIsOpen] = useState(false);  ;  
 
   return (
     <div className='main h-100vh'>
@@ -20,7 +25,83 @@ const Sidebar = () => {
 
         {/* responsive sidebar */}
         <div className={isOpen ? "sidebar show" : "sidebar close"}>
-            hello
+        <div className="container">
+          <div className='row h-20vh bg-header'>
+            <div className='header-shadow col'>
+              <div className='info row h-5vh'>
+                <div className='col align-r'>
+                <a><FaIcons.FaInfoCircle /></a>
+                </div>
+              </div>
+              <div className='row h-10vh align-center-l'>
+                <div className='col h-8vh f-poppins'>
+                  <div className='row h-5vh align-center-l'>
+                    <div className='col-2'>
+                      <img src='logoATR.png' width={35} height={35}></img>
+                    </div>
+                    <div className='rtr col-10'>Rencana Tata Ruang</div>
+                  </div>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col'>
+                  <div className='main-menu row h-5vh align-center f-14 f-roboto'>
+                    <div className='col' onClick={() => {setAddDataIsOpen(true); setLayerListIsOpen(false); setLegendIsOpen(false)}}>ADD DATA</div>
+                    <div className='col' onClick={() => {setAddDataIsOpen(false); setLayerListIsOpen(true); setLegendIsOpen(false)}}>LAYER LIST</div>
+                    <div className='col' onClick={() => {setAddDataIsOpen(false); setLayerListIsOpen(false); setLegendIsOpen(true)}}>LEGEND</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          { addDataIsOpen &&
+          <div className="row h-80vh">
+            <div className='col'>
+              <div className='row sub-menu'>
+                <div className='col-10'>Rencana Tata Ruang (RTR)</div>
+                <div className='col-2'><FaIcons.FaCaretDown /></div>
+              </div>
+              <div className='row sub-menu'>
+                <div className='col-10'>Simpul</div>
+                <div className='col-2'><FaIcons.FaCaretDown /></div>
+              </div>
+              <div className='row sub-menu'>
+                <div className='col-10'>Cari</div>
+                <div className='col-2'><FaIcons.FaCaretDown /></div>
+              </div>
+              <div className='row sub-menu'>
+                <div className='col-10'>URL</div>
+                <div className='col-2'><FaIcons.FaCaretDown /></div>
+              </div>
+              <div className='row sub-menu'>
+                <div className='col-10'>File</div>
+                <div className='col-2'><FaIcons.FaCaretDown /></div>
+              </div>
+            </div>
+          </div>}
+          { layerListIsOpen &&
+          <div className="row h-80vh">
+            <div className='col'>
+              <div className='row'>
+                <div>MENU LAYER LIST 1</div>
+              </div>
+              <div className='row'>
+                <div>MENU LAYER LIST 2</div>
+              </div>
+            </div>
+          </div>}
+          { legendIsOpen &&
+          <div className="row h-80vh">
+            <div className='col'>
+              <div className='row'>
+                <div>MENU LEGEND 1</div>
+              </div>
+              <div className='row'>
+                <div>MENU LEGEND 2</div>
+              </div>
+            </div>
+          </div>}
+        </div>
         </div>
         <td className={isOpen ? "collapse-btn show" : "collapse-btn close"} onClick={() => {setIsOpen(!isOpen); collapsed()}}>
           <a><FaIcons.FaCaretDown /></a>
